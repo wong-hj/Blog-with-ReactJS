d9d227bbca9834608401d4afa02741f61fdea371
june <hj.wong02@gmail.com>
2021-12-03 18:50:22 +0800

    added styles and blog contents

--- june-blog/src/App.js
+++ june-blog/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+
 import Navbar from './navbar';
 import Home from './home';
 
--- june-blog/src/home.js
+++ june-blog/src/home.js
@@ -1,9 +1,42 @@
+import { useState } from 'react';
+
+// const Home = () => {
+
+//     const [name, setName] = useState('June');
+//     const [age, setAge] = useState(25);
+
+//     const handleClick = () => {
+//         setName('June2');
+//         setAge(55);
+//     } 
+
+//     return ( 
+//         <div className="home">
+//             <h2>Homepage</h2>
+//             <p>{name} is {age} years old</p>
+//             <button onClick = {handleClick}>Click Yes</button>
+            
+//         </div>
+//      );
+// }
+
 const Home = () => {
+
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
+
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
      );
 }
- 
 export default Home
