56e6a5e5ba5138b0ff54459344eb399f26fdbf2f
june <hj.wong02@gmail.com>
2021-12-03 18:07:39 +0800

    added other components

--- june-blog/src/App.js
+++ june-blog/src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './navbar';
+import Home from './home';
 
 function App() {
+  const title = "Welcome to the new blog";
+  const likes = 50;
+  const link = "http://www.google.com";
+  // const person = { name: "Yoshi", age: 30};
+
   return (
     <div className="App">
-      
+      <Navbar></Navbar>
+      <div className="content">
+        <Home></Home>
+      </div>
     </div>
   );
 }
