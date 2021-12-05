4dcb8db5b5a95a3a1a790a80bb30f6d2d998ae23
june <hj.wong02@gmail.com>
2021-12-06 00:14:01 +0800

    Added Custom Hooks

--- june-blog/src/home.js
+++ june-blog/src/home.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 // const Home = () => {
 
@@ -23,32 +24,11 @@ import BlogList from './BlogList';
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-
-
-
-
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                console.log(res);
-                if(res.ok) {
-
-                }
-                return res.json();
-            })
-            .then((data) => {
-                setBlogs(data);
-                setIsPending(false);
-            })
-            .catch(err => {
-                console.log(err.message);
-            })
-    }, []);
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
     return ( 
         <div className="home">
+            { error && <div>{ error } </div> }
             {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!"></BlogList>}
 
