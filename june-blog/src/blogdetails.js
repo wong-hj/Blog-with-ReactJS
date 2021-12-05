a88df571836512a7287565b7832e9ddf61b82fdc
june <hj.wong02@gmail.com>
2021-12-06 01:51:54 +0800

    Added Post, Delete blog functions

--- june-blog/src/blogdetails.js
+++ june-blog/src/blogdetails.js
@@ -1,9 +1,20 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import {useHistory} from 'react-router-dom';
+
 
 const BlogDetails = () => {
     const { id } = useParams();
     const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs/' + id);
+    const history = useHistory();
+
+    const handleClick= () => {
+        fetch('http://localhost:8000/blogs/' + blogs.id, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
+        })
+    }
 
     return ( 
         <div className="blog-details">
@@ -14,6 +25,7 @@ const BlogDetails = () => {
                     <h2>{ blogs.title } </h2>
                     <p>Written by {blogs.author}</p>
                     <div className="blog-body">{blogs.body}</div>
+                    <button onClick={handleClick}>Delete Blog</button>
                 </article>
                 )}
         </div>
--- june-blog/src/create.js
+++ june-blog/src/create.js
@@ -1,7 +1,59 @@
+import { useState, useEffect } from 'react';
+import {useHistory} from 'react-router-dom';
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('');
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author};
+
+        setIsPending(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            setIsPending(false);
+            history.push('/');
+        })
+
+    }
+
+
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form onSubmit={ handleSubmit }>
+                <label>Blog Title:</label>
+                <input 
+                    type="text" 
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+
+                <label>Blog Body:</label>
+                <textarea name="" id="" required value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+
+                <label>Blog Author:</label>
+                <input 
+                    type="text" 
+                    required
+                    value={author} 
+                    onChange={(e) => setAuthor(e.target.value)}
+                />
+
+                { !isPending && <button>Add Blog</button> }
+                { isPending && <button>Adding Blog...</button> }
+
+
+            </form>
         </div>
     );
 }
